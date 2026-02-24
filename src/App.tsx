ea41834e8fb92691c9952fba1177044adee2bbf8
Andrew Cook <andrewjcook93@gmail.com>
2026-02-23 18:03:33 -0600

    add red line stations list, styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import './styles/redline.css'
 
 import Header from './components/Header.tsx'
 import BurnettArrivals from './components/BurnettArrivals.tsx'
+import RedLine from './components/RedLine.tsx'
 
 function App() {
 
@@ -10,6 +12,7 @@ function App() {
       <Header />
       <h1>Burnett Transit Center Arrivals</h1>
       <BurnettArrivals />
+      <RedLine />
     </>
   )
 }
---
+++ src/data/redline.ts
@@ -0,0 +1,35 @@
+// Define a type for each stop
+export interface Stop {
+  name: string;
+  description?: string;
+  img?: string;
+  popularLocations?: string[];
+}
+
+const redline_stops = [
+  { name: "Northline Transit Center/HCC" },
+  { name: "Melbourne/North Lindale" },
+  { name: "Lindale Park" },
+  { name: "Cavalcade" },
+  { name: "Moody Park" },
+  { name: "Fulton/North Central" },
+  { name: "Quitman/Near Northside" },
+  { name: "UH-Downtown" },
+  { name: "Preston" },
+  { name: "Central Station Main (Transfer to Green/Purple Lines)" },
+  { name: "Main Street Square" },
+  { name: "Bell" },
+  { name: "McGowen" },
+  { name: "Ensemble/HCC" },
+  { name: "Wheeler" },
+  { name: "Museum District" },
+  { name: "Hermann Park/Rice U" },
+  { name: "Memorial Hermann Hospital/Houston Zoo" },
+  { name: "Dryden/TMC" },
+  { name: "TMC Transit Center" },
+  { name: "Smith Lands" },
+  { name: "Stadium Park/Astrodome" },
+  { name: "Fannin South" }
+];
+
+export default redline_stops;
